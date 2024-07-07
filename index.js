69214e04abfde9d7b058e28b3935b4c845359091
JoyMaveke <Mavekejoy@gmail.com>
2024-07-07 18:57:52 +0300

    list edits

---
+++ index.js
@@ -0,0 +1,11 @@
+let items = JSON.parse(localStorage.getItem('items'))  || []
+
+// variables that will remain constant.
+const shoppingList = document.getElementById('shoppinglist')
+const textInput = document.getElementById('textinput');
+const addBtn = document.getElementById('add-btn');
+const clearBtn = document.getElementById('clear-btn');
+const markPurchasedBtn = document.getElementById('markPurchasedButton');
+
+// Making text area clear after clicking add item.
+
