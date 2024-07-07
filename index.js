5aa09aed9f5acac8f7e98e8908180aa382f3596b
JoyMaveke <Mavekejoy@gmail.com>
2024-07-07 23:46:26 +0300

    js changes

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
-let items = JSON.parse(localStorage.getItem('items'))  || []
+let listItems = [];
 
-// variables that will remain constant.
-const shoppingList = document.getElementById('shoppinglist')
-const textInput = document.getElementById('textinput');
-const addBtn = document.getElementById('add-btn');
-const clearBtn = document.getElementById('clear-btn');
-const markPurchasedBtn = document.getElementById('markPurchasedButton');
+//constant values
+const itemInput = document.querySelector('input[name="New Item"]');
+const list = document.querySelector('.list-container ul');
+const addBtn = document.querySelector('#add-btn');
+const clearBtn = document.querySelector('#clear-btn');
+const purchasedBtn = document.querySelector('#mark-btn')
 
 
+//Event Listeners
+list.addEventListener('click', appendListItemClick)
+addBtn.addEventListener('click', appendItem);
+clearBtn.addEventListener('click', appendClearList);
+purchasedBtn.addEventListener('click', appendPurchaseItem);
+
+
+//Appending
+function appendListItemClick(event){
+    if(EventTarget === LI){
+        const selectedItem = getSelectedItem(EventTarget);
+        if (selectedItem) {
+            selectedItem.purchased = !selectedItem.purchased;
+            renderList();
+    
+        }}
+    }
+
+function appendItem() {
+    const newItem = itemInput.value.trim();
+        if (newItem) {
+           listItems.push({ item: newItem, purchased: false });
+           itemInput.value = '';
+           renderList();
+            }
+}
+
+function appendClearList() {
+    listItems = [];
+    renderList();
+  }
+
+function appendPurchaseItem(){
+    const selectedItem = getSelectedItem();
+  if (selectedItem) {
+    selectedItem.purchased = true;
+    renderList();
+  }
+}
+
+function renderList() {
+    list.innerHTML = listItems.map((item , index) =>`
+        <li${item.purchased ? 'class="purchased"' : ''}>${item.item}</li>`).join('');
+}
+
+        
