9c38a72f22398b84f0db7de3c50c1f289a9a98fa
JoyMaveke <Mavekejoy@gmail.com>
2024-07-09 22:34:29 +0300

    readme addition

--- index.js
+++ index.js
@@ -15,4 +15,40 @@ purchasedBtn.addEventListener('click', appendPurchaseItem);
 
 
 //Appending
-        
+
+function createList() {
+    list.innerHTML = '';
+    for (let i = 0; i < listItems.length; i++) {
+      let item = listItems[i];
+      let li = document.createElement('li');
+      li.textContent = item.item;
+      if (item.purchased) {
+        li.className = 'purchased';
+      }
+      list.appendChild(li);
+    }
+  }
+
+function appendItem() {
+    const newItem = itemInput.value.trim();
+    if (newItem) {
+    listItems.push({ item: newItem, purchased: false });
+    itemInput.value = '';
+    createList();
+    }
+    }
+    
+    function appendClearList() {
+    listItems = [];
+    createList();
+    }
+    
+    function appendPurchaseItem(){
+    const selectedItem = getSelectedItem();
+    if (selectedItem) {
+    selectedItem.purchased = true;
+    createList();
+    }
+    }
+    
+   
