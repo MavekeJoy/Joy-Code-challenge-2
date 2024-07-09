1809ee6ae35df1c25799e801241c8bb57c123882
JoyMaveke <Mavekejoy@gmail.com>
2024-07-09 22:34:25 +0300

    readme addition

--- index.js
+++ index.js
@@ -9,48 +9,10 @@ const purchasedBtn = document.querySelector('#mark-btn')
 
 
 //Event Listeners
-list.addEventListener('click', appendListItemClick)
 addBtn.addEventListener('click', appendItem);
 clearBtn.addEventListener('click', appendClearList);
 purchasedBtn.addEventListener('click', appendPurchaseItem);
 
 
 //Appending
-function appendListItemClick(event){
-    if(EventTarget === LI){
-        const selectedItem = getSelectedItem(EventTarget);
-        if (selectedItem) {
-            selectedItem.purchased = !selectedItem.purchased;
-            renderList();
-    
-        }}
-    }
-
-function appendItem() {
-    const newItem = itemInput.value.trim();
-        if (newItem) {
-           listItems.push({ item: newItem, purchased: false });
-           itemInput.value = '';
-           renderList();
-            }
-}
-
-function appendClearList() {
-    listItems = [];
-    renderList();
-  }
-
-function appendPurchaseItem(){
-    const selectedItem = getSelectedItem();
-  if (selectedItem) {
-    selectedItem.purchased = true;
-    renderList();
-  }
-}
-
-function renderList() {
-    list.innerHTML = listItems.map((item , index) =>`
-        <li${item.purchased ? 'class="purchased"' : ''}>${item.item}</li>`).join('');
-}
-
         
